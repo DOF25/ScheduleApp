991b8b74887f7e1660b38a82dba51c9f179954c2
Danila Krylov <danila.krylov.1980@mail.ru>
2022-01-01 07:11:59 +0300

    Initial Commit

---
+++ ScheduleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScheduleApp
+//
+//  Created by Крылов Данила  on 01.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
